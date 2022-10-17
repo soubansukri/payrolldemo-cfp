12e6abfb0b029651d814d4445222ad196d8b9c74
soubansukri <souban.sukri@gmail.com>
2022-10-18 02:14:03 +0530

    added:logic to validate name & salary

--- employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/repo/IEmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.employeepayrollapp.repo;
-
-import com.bridgelabz.employeepayrollapp.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IEmployeeRepository extends JpaRepository<Employee,Long> {
-}
